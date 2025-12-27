a87eca21188abc87adb5356536bd8a43cbb89a74
Yeray Zalaya <yerayzalaya@gmail.com>
2025-12-28 00:32:53 +0100

    BD es fa una cirurgia plàstica

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,3 +1,14 @@
-import { PrismaClient } from '@/lib/prisma/client'
+// lib/prisma.ts
+import { PrismaClient } from '@prisma/client'
 
-export const prisma = new PrismaClient()
+// Afegim prisma a l'objecte global de NodeJS per evitar
+// múltiples instàncies durant el "hot reloading" en desenvolupament.
+const globalForPrisma = global as unknown as { prisma: PrismaClient }
+
+export const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    log: ['query'], // Opcional: Això farà que vegis les consultes SQL a la consola (molt útil per aprendre)
+  })
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
