fd7789072b6d0a43f0836f4a5d60db90a00d78ab
Yeray Zalaya <yerayzalaya@gmail.com>
2025-12-28 01:13:34 +0100

    prisma update 7.x

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,14 +1,22 @@
-// lib/prisma.ts
+// src/lib/prisma.ts
 import { PrismaClient } from '@prisma/client'
+import { Pool } from 'pg'
+import { PrismaPg } from '@prisma/adapter-pg'
 
-// Afegim prisma a l'objecte global de NodeJS per evitar
-// múltiples instàncies durant el "hot reloading" en desenvolupament.
+// 1. Configurem la connexió amb el driver natiu de Postgres (pg)
+const connectionString = process.env.DATABASE_URL
+
+const pool = new Pool({ connectionString })
+const adapter = new PrismaPg(pool)
+
+// 2. Mantenim la lògica del global per evitar problemes amb el hot-reloading de Next.js
 const globalForPrisma = global as unknown as { prisma: PrismaClient }
 
 export const prisma =
   globalForPrisma.prisma ||
   new PrismaClient({
-    log: ['query'], // Opcional: Això farà que vegis les consultes SQL a la consola (molt útil per aprendre)
+    adapter, // <--- AQUÍ ESTÀ LA CLAU: Passem l'adaptador explícitament
+    log: ['query'], 
   })
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
